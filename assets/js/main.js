878ce18fd09317969a81b3530c19efde86818d88
GoncaloGMendonca <goncalogm2002@gmail.com>
2023-09-13 11:08:35 +0100

    pequenos Fixes no java

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,3 +122,104 @@ radioButtons.forEach(radioButton => {
     });
   });
 });
+
+
+
+
+
+
+// sistema de pausar os video no modal no hover no rato
+function setupCarousels() {
+  var carousels = document.querySelectorAll('.carousel');
+
+  for (var i = 0; i < carousels.length; i++) {
+    var carousel = carousels[i];
+
+    $(carousel).carousel(); // set up the carousel
+
+    // Pause the carousel when the mouse is over it
+    carousel.addEventListener('mouseover', function() {
+      $(this).carousel('pause'); // pause the carousel
+    });
+
+    // Resume the carousel when the mouse leaves it
+    carousel.addEventListener('mouseout', function() {
+      $(this).carousel('cycle'); // resume the carousel
+    });
+  }
+}
+
+window.onload = function() {
+  setupCarousels();
+};
+
+// sistema de pausar os video no modal no slide over
+function setupCarousels() {
+  var carousels = document.querySelectorAll('.carousel');
+
+  for (var i = 0; i < carousels.length; i++) {
+    var carousel = carousels[i];
+
+    $(carousel).carousel(); // set up the carousel
+
+    carousel.addEventListener('slide.bs.carousel', function(event) {
+      var currentSlide = event.from; // get the current slide index
+      var slideItems = this.querySelectorAll('.carousel-item'); // get all slide items
+      var slideVideos = slideItems[currentSlide].querySelectorAll('video'); // get all videos in the current slide
+      var slideIFrames = slideItems[currentSlide].querySelectorAll('iframe'); // get all iframes in the current slide
+
+      // Pause all videos in the current slide
+      for (var j = 0; j < slideVideos.length; j++) {
+        if (!slideVideos[j].paused) {
+          slideVideos[j].pause();
+        }
+      }
+
+      // Pause all iframes in the current slide
+      for (var k = 0; k < slideIFrames.length; k++) {
+        var iframeSrc = slideIFrames[k].src;
+        slideIFrames[k].src = iframeSrc; // reload the iframe to pause it
+      }
+    });
+  }
+}
+
+window.onload = function() {
+  setupCarousels();
+};
+
+
+
+
+  // get all modal elements
+var modalElements = document.querySelectorAll('.modal');
+
+// loop through each modal element
+modalElements.forEach(function(modalElement) {
+
+  // get the video element
+  var iframeElement = modalElement.querySelector('iframe');
+
+  // add event listener for when the modal is shown
+  modalElement.addEventListener('shown.bs.modal', function() {
+
+    // set the src attribute of the iframe to start the video
+    if (iframeElement) {
+      var iframeSrc = iframeElement.getAttribute('src');
+      iframeElement.setAttribute('src', iframeSrc + '?autoplay=0');
+    }
+  });
+
+  // add event listener for when the modal is hidden
+  modalElement.addEventListener('hidden.bs.modal', function() {
+
+    // set the src attribute of the iframe to stop the video
+    if (iframeElement) {
+      var iframeSrc = iframeElement.getAttribute('src');
+      iframeElement.setAttribute('src', iframeSrc.replace('?autoplay=0', ''));
+    }
+  });
+
+});
+
+
